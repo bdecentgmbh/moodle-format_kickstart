3038033c433f0331886f96be68345f97fca1bd15
moodledev <moodledev@gmail.com>
2019-06-18 12:16:32 -0500

    Add maturity and release

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2019061703;         // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018112800;         // Requires this Moodle version.
 $plugin->component = 'format_kickstart'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0';
