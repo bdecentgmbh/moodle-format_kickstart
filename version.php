02d258756f257650ec714a1766b16a38fa1be213
moodledev <moodledev@gmail.com>
2020-06-13 17:08:13 -0500

    Bump version

--- version.php
+++ version.php
@@ -28,4 +28,4 @@ $plugin->version   = 2020061300;         // The current plugin version (Date: YY
 $plugin->requires  = 2017051509;         // Requires this Moodle version.
 $plugin->component = 'format_kickstart'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.1.4';
+$plugin->release   = '1.1.5';
