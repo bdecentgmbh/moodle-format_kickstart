bfe549c76a61cde6ce29a17d318f5a1c185bec44
Nithy1anandan <nithyanandan.m.2016@lmsace.com>
2021-09-21 13:14:58 +0530

    Upgrade plugin savepoint version upgraded

--- db/upgrade.php
+++ db/upgrade.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Upgrade script for format_weeks
+ * Upgrade script for format_kickstart
  *
  * @param int $oldversion the version we are upgrading from
  * @return bool result
@@ -162,7 +162,7 @@ function xmldb_format_kickstart_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2020052502, 'format', 'kickstart');
     }
 
-    if ($oldversion < 2021091600) {
+    if ($oldversion < 2021092100) {
         // Define field sort to be added to format_kickstart_template.
         $table = new xmldb_table('format_kickstart_template');
         $field = new xmldb_field('sort', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'roleids');
@@ -171,7 +171,7 @@ function xmldb_format_kickstart_upgrade($oldversion) {
         }
 
         // Kickstart savepoint reached.
-        upgrade_plugin_savepoint(true, 2021091600, 'format', 'kickstart');
+        upgrade_plugin_savepoint(true, 2021092100, 'format', 'kickstart');
     }
 
     return true;
