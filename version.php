c8cf0f6df79ed856c8791e5700f44d530224bb45
Prasanna LMSACE <prasanna_t@lmsace.com>
2023-03-23 14:29:08 +0530

    Fixed the kickstart 1.3 task issues.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023032301;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2023032300;         // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2022030300;         // Requires this Moodle version.
 $plugin->release = 'Version 1.2';
 $plugin->component = 'format_kickstart'; // Full name of the plugin (used for diagnostics).
