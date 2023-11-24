32ee95f90674bd41f3c74f09a1ca2849107e5063
Stefan-Alexander Scholz <hello@bdecent.de>
2023-11-24 11:50:01 +0100

    Update version.php

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023071201;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2023071202;         // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2022041900;         // Requires this Moodle version.
 $plugin->release = 'Version 1.3';
 $plugin->component = 'format_kickstart'; // Full name of the plugin (used for diagnostics).
