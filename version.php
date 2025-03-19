d43664cf010dbcfc7e0d2feb2cb9fca5a8628d66
Nithyanandan <nithyanandan.m.2016@lmsace.com>
2025-03-19 18:32:17 +0530

    version file updated

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025031900;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2025031901;         // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2022041900;         // Requires this Moodle version.
 $plugin->release = 'Version 1.4';
 $plugin->component = 'format_kickstart'; // Full name of the plugin (used for diagnostics).
