887f1887a191b0fe0c48316f9e1f808c3200dba6
Nithyanandan <nithyanandan.m.2016@lmsace.com>
2025-03-22 12:56:42 +0530

    The style alignment issues reported in the ClickUp bug have been fixed. Version file updated

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025031901;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2025032200;         // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2022041900;         // Requires this Moodle version.
 $plugin->release = 'Version 1.4';
 $plugin->component = 'format_kickstart'; // Full name of the plugin (used for diagnostics).
-$plugin->supported = [400, 405];
+$plugin->supported = [400, 403];
 $plugin->maturity = MATURITY_STABLE;
